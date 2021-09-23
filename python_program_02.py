f281f28e975bc2b61054fdaf7d05d9485f63ca99
slynch66 <55889829+slynch66@users.noreply.github.com>
2021-09-23 11:33:28 -0400

    Update python_program_02.py

--- python_program_02.py
+++ python_program_02.py
@@ -5,19 +5,9 @@
 #9.21.2021
 from math import sqrt # this will allow me to use the 
 print("Quadratic Solver")
-print("Enter the coefficients for ax^2 + bx + c = 0")
 
-def quadSolver(a,b,c):
-    discriminant = b*b - 4 * a * c
-    if discriminant < 0:
-        roots = ""
-        return roots
-    else: 
-        x = sqrt(b*b - 4*a*c)
-        root1 = (-b + x)/(2*a)
-        root2 = (-b - x)/(2*a)
-        roots = [root1, root2]
-        return roots
+retry = input("press Enter to start")
+
 
 # These two lines ask the user whatever is inside the quotation markes and then
 # takes their response and makes that the a or b variable. Because you have a/b
@@ -27,15 +17,34 @@ a = int(input("Enter the 'a' coefficient: "))
 b = int(input("Enter the 'b' coefficient: "))
 c = int(input("Enter the 'c' coefficient: "))
 
+# The while loop makes the "Press Enter to start" work and it also makes
+# the line below work also:
+#       retry = input("press Enter to start or 'x' then Enter to quit: ")
+
+while retry == "":
+    print("Enter the coefficients for ax^2 + bx + c = 0")
+
+    def quadSolver(a,b,c):
+            
 # [The discriminant variable] takes the discriminant of the quadratic equation.
 # You can't use the ^ symbol to give something an exponent, you have to multiply
 # it by itself : b*b.
-
-roots = quadSolver(a,b,c)
-if roots == "":
-    print("There are no real roots.")
-else: 
-    print("Root #1: ")
-    print(roots[0])
-    print("Root #2: ")
-    print(roots[1])
+        discriminant = b*b - 4 * a * c
+        if discriminant < 0:
+            roots = ""
+            return roots
+        else: 
+            x = sqrt(b*b - 4*a*c)
+            root1 = (-b + x)/(2*a)
+            root2 = (-b - x)/(2*a)
+            roots = [root1, root2]
+            return roots
+    roots = quadSolver(a,b,c)
+    if roots == "":
+        print("There are no real roots.")
+    else: 
+        print("Root #1: ")
+        print(roots[0])
+        print("Root #2: ")
+        print(roots[1])
+    retry = input("press Enter to start or 'x' then Enter to quit: ")
