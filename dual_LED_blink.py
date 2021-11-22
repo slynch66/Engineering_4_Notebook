bae893e85099ddc95980747313c289c0d0a6258a
slynch66 <55889829+slynch66@users.noreply.github.com>
2021-11-22 11:38:08 -0500

    Update dual_LED_blink.py

--- dual_LED_blink.py
+++ dual_LED_blink.py
@@ -9,14 +9,15 @@ from time import sleep
 
 GPIO.setmode(GPIO.BCM) #this sets my pin numbering scheme as BCM
 
-GPIO.setup(21, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(21, GPIO.OUT, initial=GPIO.LOW)  #this sets pin 20 and 21 as outputs, and sets the output to no power
 GPIO.setup(20, GPIO.OUT, initial=GPIO.LOW)
+# This while loop makes the LEDs alternate between red and blue being on
 while True:
 	GPIO.output(21, GPIO.HIGH)
 	GPIO.output(20, GPIO.LOW)
 	print("red on, blue off")
-	sleep(1)
+	sleep(1)                         # wait one full second
 	GPIO.output(21, GPIO.LOW)
 	GPIO.output(20, GPIO.HIGH)
 	print("red off, blue on")
-	sleep(1)
+	sleep(1)                         # wait one full second
