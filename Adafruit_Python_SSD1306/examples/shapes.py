10b5aed38e01ee5e445bdb279d9988798fb83017
slynch66 <slynch66@charlottesvilleschools.org>
2021-12-09 17:25:36 +0000

    Demo

--- Adafruit_Python_SSD1306/examples/shapes.py
+++ Adafruit_Python_SSD1306/examples/shapes.py
@@ -49,7 +49,7 @@ SPI_DEVICE = 0
 #disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
 
 # Note you can change the I2C address by passing an i2c_address parameter like:
-	disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3d)
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3d)
 
 # Alternatively you can specify an explicit I2C bus number, for example
 # with the 128x32 display you would use:
